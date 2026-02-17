ba48590755f5039ca99e4bafeda2da02bd16989d
mindspringpath <mindspringpath@gmail.com>
2026-02-18 05:50:25 +0800

    Remove .next from repo

--- .next/static/chunks/app/program/page-e182ec93507373fc.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[418,301,951,469],{4611:function(e,n,s){Promise.resolve().then(s.bind(s,3271)),Promise.resolve().then(s.t.bind(s,231,23))}},function(e){e.O(0,[149,434,271,971,23,744],function(){return e(e.s=4611)}),_N_E=e.O()}]);
